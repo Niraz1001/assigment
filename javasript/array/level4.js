347508cf23657f1400e47cc0526194cc4459edfa
Niraz1001 <nk9creation@gmail.com>
2024-08-04 07:39:36 +0545

    soin

--- javasript/array/level4.js
+++ javasript/array/level4.js
@@ -34,5 +34,38 @@ console.log(total);
 
 
 
+// high level
+// Q3 
+// expected output: 12
+// count total characters in all the strings in the array
+let strCount = 0
+arr.forEach((item,id)=>{
+   strCount = strCount + item.length;
+})
+console.log(strCount);
 
 
+
+// Q4 using map, get the last characters
+const lastOutput = arr.map((item)=>{
+
+    return item[item.length - 1]
+    
+})
+console.log(lastOutput);
+//['m','m','i']
+
+
+
+const cartItems=[
+    {id: 1, product:"hawkins", price:30, quantity:1},
+    {id: 1, product:"baltra", price:10, quantity:3},
+    ]
+let totalPrice = 0
+cartItems.forEach((item)=>{
+    let total = item.price * item.quantity;
+    totalPrice= totalPrice + total ;
+})
+console.log(totalPrice)
+
+    //60
